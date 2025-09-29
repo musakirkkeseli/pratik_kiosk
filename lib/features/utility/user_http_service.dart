91bf181d0481712dd8ec195fd6a2c0f6aff5bc47
musakirkkeseli <musakirkkeseli@gmail.com>
2025-09-29 13:46:23 +0300

    UserLoginStatusService kullancak şekilde düzenlendi

--- lib/features/utility/user_http_service.dart
+++ lib/features/utility/user_http_service.dart
@@ -1,27 +1,27 @@
 import 'package:dio/dio.dart';
 
 import '../../core/utility/http_service.dart';
-import '../../core/utility/cache_manager.dart';
-import '../../core/utility/login_status_service.dart';
+import '../../core/utility/user_login_status_service.dart';
 
 class UserHttpService extends HttpService {
-  UserHttpService({
-    String? baseUrl,
-  }) : super.withInterceptor(
-          interceptor: InterceptorsWrapper(
-            onRequest: (options, handler) async {
-              final token = await CacheManager().readString('token');
-              if (token != null && token.isNotEmpty && options.headers['Authorization'] == null) {
-                options.headers['Authorization'] = 'Bearer $token';
-              }
-              handler.next(options);
-            },
-            onError: (DioException e, handler) async {
-              if (e.response?.statusCode == 401) {
-                LoginStatusService().logout();
-              }
-              handler.next(e);
-            },
-          ),
-        );
+  UserHttpService({String? baseUrl})
+    : super.withInterceptor(
+        interceptor: InterceptorsWrapper(
+          onRequest: (options, handler) async {
+            final token = UserLoginStatusService().accessToken;
+            if (token != null &&
+                token.isNotEmpty &&
+                options.headers['Authorization'] == null) {
+              options.headers['Authorization'] = 'Bearer $token';
+            }
+            handler.next(options);
+          },
+          onError: (DioException e, handler) async {
+            if (e.response?.statusCode == 401) {
+              UserLoginStatusService().logout();
+            }
+            handler.next(e);
+          },
+        ),
+      );
 }
