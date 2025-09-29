23a8a59c67254ab62decce42eee6814a7ba97a75
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-09-29 13:30:50 +0300

    context hatası düzeltildi

--- lib/product/auth/patient_login/view/patient_login_view.dart
+++ lib/product/auth/patient_login/view/patient_login_view.dart
@@ -5,7 +5,9 @@ import '../../../../core/widget/snackbar_service.dart';
 import '../../../../features/utility/const/constant_string.dart';
 import '../../../../features/utility/enum/enum_general_state_status.dart';
 import '../../../../features/utility/navigation_service.dart';
-import '../../hospital_login/cubit/hospital_login_cubit.dart';
+import '../../../../features/utility/tenant_http_service.dart';
+import '../../hospital_login/services/hospital_and_user_login_services.dart';
+import '../cubit/patient_login_cubit.dart';
 import 'widget/patient_login_widget.dart';
 
 class PatientLoginView extends StatefulWidget {
@@ -18,27 +20,32 @@ class PatientLoginView extends StatefulWidget {
 class _PatientLoginViewState extends State<PatientLoginView> {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<HospitalLoginCubit, HospitalLoginState>(
-      listenWhen: (prev, curr) => prev.tcStatus != curr.tcStatus,
-      listener: (context, state) async {
-        if (state.tcStatus == EnumGeneralStateStatus.success) {
-          NavigationService.ns.routeTo("DateOfBirthWidget");
+    return BlocProvider<PatientLoginCubit>(
+      create: (context) => PatientLoginCubit(
+        service: HospitalAndUserLoginServices(TenantHttpService()),
+      ),
+      child: BlocConsumer<PatientLoginCubit, PatientLoginState>(
+        listenWhen: (prev, curr) => prev.tcStatus != curr.tcStatus,
+        listener: (context, state) async {
+          if (state.tcStatus == EnumGeneralStateStatus.success) {
+            NavigationService.ns.routeTo("DateOfBirthWidget");
 
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(const SnackBar(content: Text('TC doğrulandı.')));
-        } else if (state.tcStatus == EnumGeneralStateStatus.failure) {
-          SnackbarService().showSnackBar(
-            state.message ?? ConstantString().errorOccurred,
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(const SnackBar(content: Text('TC doğrulandı.')));
+          } else if (state.tcStatus == EnumGeneralStateStatus.failure) {
+            SnackbarService().showSnackBar(
+              state.message ?? ConstantString().errorOccurred,
+            );
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(title: Text(ConstantString().patientLogin)),
+            body: PatientLoginWidget(),
           );
-        }
-      },
-      builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(title: Text(ConstantString().patientLogin)),
-          body: PatientLoginWidget(),
-        );
-      },
+        },
+      ),
     );
   }
 }
