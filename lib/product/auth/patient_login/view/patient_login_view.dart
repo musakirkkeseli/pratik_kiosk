65f9141291d8e459ada022021885bd61a677120f
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-09-30 09:29:53 +0300

    patient_view

--- lib/product/auth/patient_login/view/patient_login_view.dart
+++ lib/product/auth/patient_login/view/patient_login_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kiosk/product/auth/patient_login/services/patient_services.dart';
 
 import '../../../../core/widget/snackbar_service.dart';
 import '../../../../features/utility/const/constant_string.dart';
 import '../../../../features/utility/enum/enum_general_state_status.dart';
 import '../../../../features/utility/navigation_service.dart';
 import '../../../../features/utility/tenant_http_service.dart';
-import '../../hospital_login/services/hospital_and_user_login_services.dart';
 import '../cubit/patient_login_cubit.dart';
 import 'widget/patient_login_widget.dart';
 
@@ -21,22 +21,25 @@ class _PatientLoginViewState extends State<PatientLoginView> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<PatientLoginCubit>(
-      create: (context) => PatientLoginCubit(
-        service: HospitalAndUserLoginServices(TenantHttpService()),
-      ),
+      create: (context) =>
+          PatientLoginCubit(service: PatientServices(TenantHttpService())),
       child: BlocConsumer<PatientLoginCubit, PatientLoginState>(
         listenWhen: (prev, curr) => prev.tcStatus != curr.tcStatus,
         listener: (context, state) async {
           if (state.tcStatus == EnumGeneralStateStatus.success) {
-            NavigationService.ns.routeTo("DateOfBirthWidget");
-
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(const SnackBar(content: Text('TC doğrulandı.')));
+            // TC kayıtlı → AppointmentView’a yönlendir
+            // NavigationService.ns.routeTo("AppointmentsView");
+            // ScaffoldMessenger.of(
+            //   context,
+            // ).showSnackBar(const SnackBar(content: Text('TC doğrulandı.')));
           } else if (state.tcStatus == EnumGeneralStateStatus.failure) {
-            SnackbarService().showSnackBar(
-              state.message ?? ConstantString().errorOccurred,
-            );
+            if (state.authType == AuthType.register) {
+              NavigationService.ns.routeTo("DateOfBirthWidget");
+            } else {
+              SnackbarService().showSnackBar(
+                state.message ?? ConstantString().errorOccurred,
+              );
+            }
           }
         },
         builder: (context, state) {
