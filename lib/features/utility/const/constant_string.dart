65580f2c40569845bccd7d1bb3d6b9f762c13958
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-10-01 09:30:47 +0300

    Const

--- lib/features/utility/const/constant_string.dart
+++ lib/features/utility/const/constant_string.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'environment.dart';
-import 'locale_keys.g.dart';
 
 class ConstantString {
   static var backendUrl = Environment.backendUrl;
-   String errorOccurred = "errorOccurred";
+  String errorOccurred = "errorOccurred";
+  String notFoundAppointment = "Randebu Bulunmamaktadır";
 
-
-     // Localization
+  // Localization
   // ignore: constant_identifier_names
   static const SUPPORTED_LOCALE = [
     ConstantString.TR_LOCALE,
     ConstantString.EN_LOCALE,
-    ConstantString.AR_LOCALE
+    ConstantString.AR_LOCALE,
   ];
   static const TR_LOCALE = Locale("tr", "TR");
   static const EN_LOCALE = Locale('en', 'EN');
@@ -23,9 +22,12 @@ class ConstantString {
   static const english = "English";
   static const arabic = "عربي";
 
-
   String birthDate = "Doğum Tarihi";
   String signIn = "Giriş Yap";
   String patientLogin = "Hasta Girişi";
   String hospitalLogin = "Hastane Girişi";
+  String appointments = "Randevular";
+  String logout = "Çıkış";
+  String home = "Ana Sayfa";
+  String selectBranch = "Bölüm Seçiniz";
 }
