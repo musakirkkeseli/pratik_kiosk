b0a904211c1add285af39666cc268c9f577dc3bf
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-10-06 09:38:07 +0300

    loading warning widget

--- lib/core/widget/loading_widget.dart
+++ lib/core/widget/loading_widget.dart
@@ -1,17 +1,14 @@
-// genel loading yapısı
 import 'package:flutter/material.dart';
 
+import '../../features/utility/const/constant_string.dart';
+
 class LoadingWidget extends StatelessWidget {
-  final Color? color;
-  const LoadingWidget({
-    super.key,
-    this.color,
-  });
+  const LoadingWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator(color: color),
-    );
+        child: Image(image: AssetImage(ConstantString.loadingGif), width: 200),
+      );
   }
 }
---
+++ lib/core/widget/warning_view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class WarningScreen extends StatelessWidget {
+  final String title;
+  final String desc;
+  final String buttonText;
+  final Function()? onTap;
+
+  const WarningScreen({
+    Key? key,
+    required this.title,
+    required this.desc,
+    required this.buttonText,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.wifi_off,
+                // color: ConstColor.error,
+                size: 120,
+              ),
+              const SizedBox(height: 50),
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width * .7,
+                child: Column(
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(
+                          fontSize: 23, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      desc,
+                      style: const TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              Visibility(
+                visible: onTap != null,
+                child: InkWell(
+                  onTap: onTap,
+                  child: Container(
+                      width: MediaQuery.of(context).size.width * .8,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          // color: ConstColor.primaryColor.withOpacity(.2),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Text(
+                        buttonText,
+                        // style: const TextStyle(color: ConstColor.primaryColor),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
