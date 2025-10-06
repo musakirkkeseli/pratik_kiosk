0a9eff609d68ef69c804a0a5fa7c3afab4467594
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-10-06 09:37:29 +0300

    enum

---
+++ lib/features/utility/enum/enum_language.dart
@@ -0,0 +1,47 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../const/constant_string.dart';
+
+enum LanguageType { turkish, english, arabic }
+
+extension LanguageSelectedExtension on LanguageType {
+  LanguageType selectedLocalValue(BuildContext context) {
+    switch (context.locale) {
+      case ConstantString.TR_LOCALE:
+        return LanguageType.turkish;
+      case ConstantString.EN_LOCALE:
+        return LanguageType.english;
+      case ConstantString.AR_LOCALE:
+        return LanguageType.arabic;
+      default:
+        return LanguageType.turkish;
+    }
+  }
+}
+
+extension LanguageLocaleExtension on LanguageType {
+  Locale get localValue {
+    switch (this) {
+      case LanguageType.turkish:
+        return ConstantString.TR_LOCALE;
+      case LanguageType.english:
+        return ConstantString.EN_LOCALE;
+      case LanguageType.arabic:
+        return ConstantString.AR_LOCALE;
+    }
+  }
+}
+
+extension LanguageCountryCodeExtension on LanguageType {
+  String get countryCodeValue {
+    switch (this) {
+      case LanguageType.turkish:
+        return ConstantString.turkish;
+      case LanguageType.english:
+        return ConstantString.english;
+      case LanguageType.arabic:
+        return ConstantString.arabic;
+    }
+  }
+}
