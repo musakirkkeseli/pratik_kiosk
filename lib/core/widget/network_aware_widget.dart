9b20107aa1af67ecc4cdc3d3814d875f99c082c6
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-10-06 09:37:52 +0300

    network_aware

---
+++ lib/core/widget/network_aware_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../features/utility/const/constant_string.dart';
+import '../utility/network_status_service.dart';
+import 'warning_view.dart';
+
+
+
+class NetworkAwareWidget extends StatefulWidget {
+  final Widget? onlineChild;
+
+  const NetworkAwareWidget({Key? key, required this.onlineChild})
+      : super(key: key);
+
+  @override
+  State<NetworkAwareWidget> createState() => _NetworkAwareWidgetState();
+}
+
+class _NetworkAwareWidgetState extends State<NetworkAwareWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<NetworkStatus>(builder: (context, data, child) {
+      return data == NetworkStatus.online
+          ? widget.onlineChild ?? Container()
+          : WarningScreen(
+              title:ConstantString().internetConnectionError,
+              desc: ConstantString().checkInternetConnection,
+              buttonText: "",
+              onTap: null);
+    });
+  }
+}
