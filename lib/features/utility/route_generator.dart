f76df04dccc91621f8ccf41ac638a948f0be6bac
SuleymanTiryaki <s.fevzitiryaki@gmail.com>
2025-10-06 09:37:39 +0300

    route

--- lib/features/utility/route_generator.dart
+++ lib/features/utility/route_generator.dart
@@ -1,50 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kiosk/product/appointments/view/appointments_view.dart';
 
-import '../../product/auth/hospital_login/cubit/hospital_login_cubit.dart';
-import '../../product/auth/patient_login/view/patient_view.dart';
-import '../../product/doctor/view/doctors_view.dart';
-import '../../product/section/view/section_view.dart';
+import '../../core/widget/login_aware_widget.dart';
+import '../../product/ patient_registration_procedures/view/patient_registration_procedures_view.dart';
+import '../../product/patient_transaction/view/patient_transaction_view.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoutes(RouteSettings settings) {
     switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(
+          builder: (context) => LoginAwareWidget(),
+          settings: RouteSettings(name: settings.name),
+        );
       case "AppointmentsView":
-        // final args = settings.arguments as Map? ?? {};
         return MaterialPageRoute(
           builder: (context) => AppointmentsView(),
           settings: RouteSettings(name: settings.name),
         );
-      case "SectionSearchView":
-        return MaterialPageRoute(
-          builder: (context) => SectionSearchView(),
-          settings: RouteSettings(name: settings.name),
-        );
-
-      case "DoctorSearchView":
+      case "PatientRegistrationProceduresView":
         final args = settings.arguments as Map;
         return MaterialPageRoute(
-          builder: (context) =>
-              DoctorSearchView(sectionId: args["sectionId"] ?? 0),
+          builder: (context) => PatientRegistrationProceduresView(
+            startStep: args["startStep"],
+            showBack: args["showBack"],
+          ),
           settings: RouteSettings(name: settings.name),
         );
-
-      case "PatientView":
-        final cubit = settings.arguments as HospitalLoginCubit?;
-        if (cubit == null) {
-          return MaterialPageRoute(
-            builder: (context) => const Scaffold(),
-            settings: RouteSettings(name: settings.name),
-          );
-        }
-
+      // case "SectionSearchView":
+      //   return MaterialPageRoute(
+      //     builder: (context) => SectionSearchView(),
+      //     settings: RouteSettings(name: settings.name),
+      //   );
+      // case "DoctorSearchView":
+      //   final args = settings.arguments as Map;
+      //   return MaterialPageRoute(
+      //     builder: (context) =>
+      //         DoctorSearchView(sectionId: args["sectionId"] ?? 0),
+      //     settings: RouteSettings(name: settings.name),
+      //   );
+      case "PatientTransactionView":
         return MaterialPageRoute(
-          builder: (_) =>
-              BlocProvider.value(value: cubit, child: const PatientView()),
+          builder: (context) => PatientTransactionView(),
           settings: RouteSettings(name: settings.name),
         );
-
       default:
         return MaterialPageRoute(
           builder: (context) => const Scaffold(),
