a4225ad133abf63c022e4d0b15f85d3169f824c9
musakirkkeseli <musakirkkeseli@gmail.com>
2025-10-13 19:21:33 +0300

    clear and add HexColor

--- lib/features/utility/custom_extension.dart
+++ lib/features/utility/custom_extension.dart
@@ -1,8 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-import 'const/constant_color.dart';
-
 extension StringLocalization on String {
   String get locale => this.tr();
 
@@ -11,32 +9,9 @@ extension StringLocalization on String {
   }
 }
 
-extension ConventerDateApi on DateFormat {
-  String converterToDB(DateTime dateTime) {
-    return DateFormat("yyyy-MM-dd'T'HH:mm:ss").format(dateTime);
-  }
-
-  String converterToDate(DateTime dateTime) {
-    return DateFormat("yyyy-MM-dd").format(dateTime);
-  }
-
-  String converterToDateTR(DateTime dateTime) {
-    return DateFormat("dd-MM-yyyy").format(dateTime);
-  }
-
-  DateTime fromStringToDateTime(String dateTimeString) {
-    return DateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(dateTimeString);
-  }
-}
-
-extension PageBackgroundDecoration on BoxDecoration {
-  customLinearGradient() {
-    return const BoxDecoration(
-      gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [ConstColor.primaryColor],
-      ),
-    );
+extension HexColor on String {
+  Color toColor() {
+    final hex = replaceAll("#", this);
+    return Color(int.parse("0xFF$hex"));
   }
 }
