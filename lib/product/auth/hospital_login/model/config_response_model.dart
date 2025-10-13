63170f6f70ec9f26ab17edd3db74d98863d109bd
musakirkkeseli <musakirkkeseli@gmail.com>
2025-10-13 19:19:46 +0300

    fix ConfigResponseModel

--- lib/product/auth/hospital_login/model/config_response_model.dart
+++ lib/product/auth/hospital_login/model/config_response_model.dart
@@ -1,9 +1,27 @@
 class ConfigResponseModel {
-  String? primaryColor;
+  AppColor? color;
 
-  ConfigResponseModel({this.primaryColor});
+  ConfigResponseModel({this.color});
 
   ConfigResponseModel.fromJson(Map<String, dynamic> json) {
+    color = json['color'] != null ? AppColor.fromJson(json['color']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (color != null) {
+      data['color'] = color!.toJson();
+    }
+    return data;
+  }
+}
+
+class AppColor {
+  String? primaryColor;
+
+  AppColor({this.primaryColor});
+
+  AppColor.fromJson(Map<String, dynamic> json) {
     primaryColor = json['primaryColor'];
   }
 
