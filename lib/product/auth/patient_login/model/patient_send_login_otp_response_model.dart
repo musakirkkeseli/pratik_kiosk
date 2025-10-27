31de8c9cc8b8e03e532a0fcac1c5c6d26d28c32d
musakirkkeseli <musakirkkeseli@gmail.com>
2025-10-27 11:42:00 +0300

    remove smsResponse from PatientSendLoginOtpResponseModel

--- lib/product/auth/patient_login/model/patient_send_login_otp_response_model.dart
+++ lib/product/auth/patient_login/model/patient_send_login_otp_response_model.dart
@@ -1,63 +1,19 @@
 class PatientSendLoginOtpResponseModel {
   String? message;
   String? phoneNumber;
-  SmsResponse? smsResponse;
 
   PatientSendLoginOtpResponseModel(
-      {this.message, this.phoneNumber, this.smsResponse});
+      {this.message, this.phoneNumber});
 
   PatientSendLoginOtpResponseModel.fromJson(Map<String, dynamic> json) {
     message = json['message'];
     phoneNumber = json['phoneNumber'];
-    smsResponse = json['smsResponse'] != null
-        ? SmsResponse.fromJson(json['smsResponse'])
-        : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['message'] = message;
     data['phoneNumber'] = phoneNumber;
-    if (smsResponse != null) {
-      data['smsResponse'] = smsResponse!.toJson();
-    }
-    return data;
-  }
-}
-
-class SmsResponse {
-  Null? err;
-  Data? data;
-
-  SmsResponse({this.err, this.data});
-
-  SmsResponse.fromJson(Map<String, dynamic> json) {
-    err = json['err'];
-    data = json['data'] != null ? Data.fromJson(json['data']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['err'] = err;
-    if (this.data != null) {
-      data['data'] = this.data!.toJson();
-    }
-    return data;
-  }
-}
-
-class Data {
-  int? pkgID;
-
-  Data({this.pkgID});
-
-  Data.fromJson(Map<String, dynamic> json) {
-    pkgID = json['pkgID'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['pkgID'] = pkgID;
     return data;
   }
 }
