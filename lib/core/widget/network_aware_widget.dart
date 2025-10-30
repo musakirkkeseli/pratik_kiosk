8897c866da652713f0bedc3dd885d02aa2ee04e4
musakirkkeseli <musakirkkeseli@gmail.com>
2025-10-30 12:16:59 +0300

    fix NetworkAwareWidget

--- lib/core/widget/network_aware_widget.dart
+++ lib/core/widget/network_aware_widget.dart
@@ -5,13 +5,10 @@ import '../../features/utility/const/constant_string.dart';
 import '../utility/network_status_service.dart';
 import 'warning_view.dart';
 
-
-
 class NetworkAwareWidget extends StatefulWidget {
   final Widget? onlineChild;
 
-  const NetworkAwareWidget({Key? key, required this.onlineChild})
-      : super(key: key);
+  const NetworkAwareWidget({super.key, required this.onlineChild});
 
   @override
   State<NetworkAwareWidget> createState() => _NetworkAwareWidgetState();
@@ -20,14 +17,17 @@ class NetworkAwareWidget extends StatefulWidget {
 class _NetworkAwareWidgetState extends State<NetworkAwareWidget> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<NetworkStatus>(builder: (context, data, child) {
-      return data == NetworkStatus.online
-          ? widget.onlineChild ?? Container()
-          : WarningScreen(
-              title:ConstantString().internetConnectionError,
-              desc: ConstantString().checkInternetConnection,
-              buttonText: "",
-              onTap: null);
-    });
+    return Consumer<NetworkStatus>(
+      builder: (context, data, child) {
+        return data == NetworkStatus.online
+            ? widget.onlineChild ?? Container()
+            : WarningScreen(
+                title: ConstantString().internetConnectionError,
+                desc: ConstantString().checkInternetConnection,
+                buttonText: "",
+                onTap: null,
+              );
+      },
+    );
   }
 }
