584b8923e7434f7f7f0fd71a5798a8ff28850c39
musakirkkeseli <musakirkkeseli@gmail.com>
2025-11-10 09:59:21 +0300

    use DeviceInfoService

--- lib/product/auth/hospital_login/view/hospital_login_view.dart
+++ lib/product/auth/hospital_login/view/hospital_login_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kiosk/features/utility/enum/enum_general_state_status.dart';
 import 'package:kiosk/product/auth/hospital_login/view/widget/hospital_login_widget.dart';
 
+import '../../../../core/utility/device_info_service.dart';
 import '../../../../core/widget/snackbar_service.dart';
 import '../../../../features/utility/const/constant_string.dart';
 import '../../../../features/utility/navigation_service.dart';
@@ -20,11 +21,34 @@ class HospitalLoginView extends StatefulWidget {
 }
 
 class _HospitalLoginViewState extends State<HospitalLoginView> {
+  String? _deviceId;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadDeviceId();
+  }
+
+  Future<void> _loadDeviceId() async {
+    final deviceId = await DeviceInfoService().getDeviceId();
+    setState(() {
+      _deviceId = deviceId;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_deviceId == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return BlocProvider<HospitalLoginCubit>(
       create: (_) => HospitalLoginCubit(
-        kioskDeviceId: "lokmanEtlik",
+        kioskDeviceId: _deviceId!,
         service: HospitalAndUserLoginServices(TenantHttpService()),
       ),
       child: BlocConsumer<HospitalLoginCubit, HospitalLoginState>(
