e27134ee459faec98da7cc42dc286ffbe80202a2
musakirkkeseli <musakirkkeseli@gmail.com>
2025-12-11 10:07:06 +0300

    add color, description and message to EnumPaymentResultType

--- lib/features/utility/enum/enum_payment_result_type.dart
+++ lib/features/utility/enum/enum_payment_result_type.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../const/constant_color.dart';
 import '../const/constant_string.dart';
 
 enum EnumPaymentResultType {
@@ -10,6 +13,33 @@ enum EnumPaymentResultType {
         return ConstantString().paymentSuccess;
       case EnumPaymentResultType.failure:
         return ConstantString().paymentFailure;
+    }
+  }
+
+  MaterialColor get color {
+    switch (this) {
+      case EnumPaymentResultType.success:
+        return ConstColor.green;
+      case EnumPaymentResultType.failure:
+        return ConstColor.red;
+    }
+  }
+
+  String get description {
+    switch (this) {
+      case EnumPaymentResultType.success:
+        return ConstantString().paymentCompletedSuccessfully;
+      case EnumPaymentResultType.failure:
+        return ConstantString().paymentCompletedFailed;
+    }
+  }
+
+  String get message {
+    switch (this) {
+      case EnumPaymentResultType.success:
+        return ConstantString().examinationRegistrationCreated;
+      case EnumPaymentResultType.failure:
+        return ConstantString().requirePaymentForExaminationRegistration;
     }
   }
 }
