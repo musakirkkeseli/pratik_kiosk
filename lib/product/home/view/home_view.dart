1336be892a469bf1fa10df1004a5e364d6090e8b
musakirkkeseli <musakirkkeseli@gmail.com>
2025-12-11 10:07:38 +0300

    add SingleChildScrollView

--- lib/product/home/view/home_view.dart
+++ lib/product/home/view/home_view.dart
@@ -27,96 +27,98 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CustomAppBar(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(ConstantString().welcome, style: context.pageTitle),
-                    Text(
-                      UserLoginStatusService().fullName ?? "",
-                      style: context.pageTitle.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: ConstColor.transparent,
-                      foregroundColor: context.primaryColor,
-                      elevation: 0,
-                      side: BorderSide(color: context.primaryColor, width: 2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 32,
-                        vertical: 12,
-                      ),
-                    ),
-                    onPressed: () {
-                      UserLoginStatusService()
-                          .logout(reason: SessionEndReason.manual);
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Iconify(
-                          MaterialSymbols.exit_to_app,
-                          color: context.primaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomAppBar(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(ConstantString().welcome, style: context.pageTitle),
+                      Text(
+                        UserLoginStatusService().fullName ?? "",
+                        style: context.pageTitle.copyWith(
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          ConstantString().logout,
-                          style: context.buttonText.copyWith(
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ConstColor.transparent,
+                        foregroundColor: context.primaryColor,
+                        elevation: 0,
+                        side: BorderSide(color: context.primaryColor, width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 12,
+                        ),
+                      ),
+                      onPressed: () {
+                        UserLoginStatusService()
+                            .logout(reason: SessionEndReason.manual);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Iconify(
+                            MaterialSymbols.exit_to_app,
                             color: context.primaryColor,
-                            fontSize: 16,
                           ),
-                        ),
-                      ],
+                          Text(
+                            ConstantString().logout,
+                            style: context.buttonText.copyWith(
+                              color: context.primaryColor,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.05,
-            ),
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: EnumHomeItem.values.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    ListTile(
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: MediaQuery.of(context).size.height * 0.02,
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05,
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: EnumHomeItem.values.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        title: Text(
+                          EnumHomeItem.values[index].label,
+                          style: context.sectionTitle,
+                        ),
+                        leading: EnumHomeItem.values[index].icon(context),
+                        trailing:EnumHomeItem.values[index].trailing,
                       ),
-                      title: Text(
-                        EnumHomeItem.values[index].label,
-                        style: context.sectionTitle,
-                      ),
-                      leading: EnumHomeItem.values[index].icon(context),
-                      trailing:EnumHomeItem.values[index].trailing,
-                    ),
-                    EnumHomeItem.values[index].widget(),
-                  ],
-                );
-              },
+                      EnumHomeItem.values[index].widget(),
+                    ],
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
